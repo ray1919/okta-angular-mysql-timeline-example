4a7e01624153a2bb142a4475af175b747fff29f6
ray1919 <zzqr@163.com>
2020-03-31 15:18:10 +0800

    Angular 9 + MySQL Example

---
+++ timeline-client/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { NgxTimelineModule } from 'ngx-timeline';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { HomeComponent } from './home/home.component';
+import { TimelineComponent } from './timeline/timeline.component';
+
+import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
+
+const oktaConfig = {
+  issuer: 'https://dev-413897.okta.com/oauth2/default',
+  clientId: '0oa53ba7dJ76pOWwC4x6',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  pkce: true
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TimelineComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot(),
+    NgxTimelineModule,
+    ModalModule.forRoot(),
+    OktaAuthModule
+  ],
+  providers: [
+    { provide: OKTA_CONFIG, useValue: oktaConfig }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
